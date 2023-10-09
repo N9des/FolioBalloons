400e04c590d667ae23504f094ae566b941ca4917
Félix Riffont <igele2000@gmail.com>
2023-10-09 16:33:20 +0200

    ✨ Import GLTF + anim

---
+++ src/js/main.js
@@ -0,0 +1,169 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as dat from 'lil-gui'
+
+// import vertexShader from './shaders/vertex.glsl';
+// import fragmentShader from './shaders/fragment.glsl';
+
+export default class Sketch {
+	constructor() {
+		// Sizes
+		this.sizes = {
+			width: window.innerWidth,
+			height: window.innerHeight,
+		}
+		// Init Renderer
+		this.canvas = document.querySelector('canvas.webgl');
+
+		this.renderer = new THREE.WebGLRenderer({
+			canvas: this.canvas,
+			antialias: true
+		});
+		this.renderer.setSize(this.sizes.width, this.sizes.height);
+		this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+		// Init scene
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0x8684E4);
+		THREE.ColorManagement.enabled = false
+
+		// Init values
+		this.time = 0;
+		this.clock = new THREE.Clock();
+		this.model = null;
+
+		this.addLoader();
+
+		this.addLights();
+
+		this.addCamera();
+
+		this.addControls();
+
+		this.addMesh();
+
+		this.addDebug();
+		
+		this.render();
+		
+		// Resize
+		window.addEventListener('resize', this.resize.bind(this));
+	}
+
+	addLoader() {
+		this.loader = new GLTFLoader();
+		this.loader.load('./models/letters.glb', (gltf) => {
+			this.model = gltf.scene
+			this.scene.add(this.model);
+
+			this.addMesh()
+			this.addAnim()
+		})
+	}
+
+	addLights() {
+		const white = 0xffffff;
+		const intensity = 6;
+		this.spotLight = new THREE.SpotLight(white, intensity);
+		this.spotLight.position.set(1, 1, 1);
+		this.scene.add(this.spotLight);
+
+		this.spotLightHelper = new THREE.SpotLightHelper( this.spotLight );
+		this.scene.add( this.spotLightHelper );
+
+		const green = 0xF9F863;
+		const intensityGreen = 1.5;
+		this.pointLight = new THREE.SpotLight(white, intensityGreen);
+		this.pointLight.lookAt(0, 0, 0);
+		this.pointLight.position.set(0, -1.5, 0);
+		this.scene.add(this.pointLight);
+
+		this.pointLightHelper = new THREE.SpotLightHelper( this.pointLight );
+		this.scene.add( this.pointLightHelper );
+
+		const intensityWhite = 2;
+		this.pointLightWhite = new THREE.PointLight(white, intensityWhite);
+		this.pointLightWhite.position.set(-1, 1, 1);
+		this.scene.add(this.pointLightWhite);
+
+		this.pointLightWhiteHelper = new THREE.PointLightHelper( this.pointLightWhite );
+		this.scene.add( this.pointLightWhiteHelper );
+
+		const intensityAmbient = 2;
+		this.ambientLight = new THREE.AmbientLight(white, intensityAmbient);
+		this.scene.add(this.ambientLight);
+
+	}
+
+	addControls() {
+		this.controls = new OrbitControls(this.camera, this.canvas)
+		this.controls.enableDamping = true
+	}
+
+	addCamera() {
+		this.camera = new THREE.PerspectiveCamera(
+			70,
+			this.sizes.width / this.sizes.height,
+			0.01,
+			10
+		);
+		this.camera.position.z = 1;
+	}
+
+	addMesh() {
+		if (this.model) {
+			this.model.scale.set(0.5, 0.5, 0.5);
+			this.model.position.set(0, 0, 0);
+
+			this.model.traverse((child) => {
+				if (child instanceof THREE.Mesh) {
+					child.position.y = Math.random() * (0.1 + 0.1) - 0.1;
+				}
+			})
+		}
+	}
+
+	addDebug() {
+		const gui = new dat.GUI();
+	}
+
+	addAnim(random) {
+		const elapsedTime = this.clock.getElapsedTime();
+		if (this.model) {
+			this.model.traverse((child) => {
+				if (child instanceof THREE.Mesh) {
+					child.rotation.z = Math.sin(elapsedTime * 0.8) * 0.1;
+					child.position.y = Math.sin(elapsedTime * 0.8) * 0.1;
+				}
+			})
+		}
+	}
+
+	resize() {
+		// Update sizes
+    this.sizes.width = window.innerWidth
+    this.sizes.height = window.innerHeight
+
+    // Update camera
+    this.camera.aspect = this.sizes.width / this.sizes.height
+    this.camera.updateProjectionMatrix()
+
+    // Update renderer
+    this.renderer.setSize(this.sizes.width, this.sizes.height)
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+	}
+
+	render() {
+		this.addAnim()
+
+		// Update controls
+    this.controls.update();
+
+		this.renderer.render(this.scene, this.camera);
+		this.renderer.outputColorSpace = THREE.LinearSRGBColorSpace
+		window.requestAnimationFrame(this.render.bind(this));
+	}
+}
+
+new Sketch();
